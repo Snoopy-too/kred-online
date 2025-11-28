22163c363e3a258d452afeec27246484b7901145
joshnishikawa <joshnishikawa@gmail.com>
2025-11-29 08:30:46 +0900

    base KRED, port 3003

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/KRED/',
       server: {
-        port: 3000,
+        port: 3003,
         host: '0.0.0.0',
       },
       plugins: [react()],
